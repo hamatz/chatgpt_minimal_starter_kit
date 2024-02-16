a78452e1f5ec694556bd381c4108d4e2f0cde4a8
Makoto HAMATSU <hamatsu.m@googlemail.com>
2024-02-17 01:55:33 +0900

    プラグインの簡単なサンプルを追加

---
+++ plugin_sample/sample_plugin.py
@@ -0,0 +1,10 @@
+import flet as ft
+
+def build_ui(page: ft.Page, function_to_previous_page):
+    greeting_text = ft.Text("Welcome to the Plugin Page!", size=20)
+    back_button = ft.ElevatedButton("Back to Main Page", on_click=lambda _: function_to_previous_page(page))
+
+    # 既存のUIをクリアして、新しいUIをページに追加
+    page.clean()
+    page.add(greeting_text, back_button)
+    page.update()
