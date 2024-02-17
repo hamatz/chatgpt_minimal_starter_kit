1d026c2f8fce5835aa63dea5a9b7281ba57f5933
Makoto HAMATSU <hamatsu.m@googlemail.com>
2024-02-17 17:18:21 +0900

    メモリ周りの効率化対応
    プラグインのサンプルも合わせて修正

--- app.py
+++ app.py
@@ -2,31 +2,39 @@ import flet as ft
 from plugin_manager import PluginManager
 from my_key_manager import MyKeyManager
 
-def show_main_page(page: ft.Page) -> None:
-    def pick_file_and_install(e: ft.FilePickerResultEvent):
-        pm.install_plugin(e)
+class MyApp:
+    def __init__(self, page: ft.Page) -> None:
+        self.page = page
+        self.page.title = "ChatGPT minimal starter kit"
+        self.page.vertical_alignment = ft.MainAxisAlignment.START
 
-    page.title = "ChatGPT minimal starter kit"
-    page.vertical_alignment = ft.MainAxisAlignment.START
+        # インスタンスの初期化
+        self.mkm = MyKeyManager(self.page)
+        self.pm = PluginManager(self.page, self.page_back)
 
-    mkm = MyKeyManager(page)
-    mkm.load_my_key()
-    pm = PluginManager(page, page_back)
-    # インストール済みのプラグインを読み込む
-    pm.load_installed_plugins()
-    # プラグインをインストールするボタンを表示する
-    file_picker = ft.FilePicker(on_result=pick_file_and_install)
-    page.overlay.append(file_picker)
-    install_button = ft.ElevatedButton("Install Plugin", icon=ft.icons.UPLOAD_FILE, on_click=lambda _:file_picker.pick_files())
-    page.add(install_button)
-    page.update()
+        # インスタンスを再利用するために、初期化をここで一度だけ実行
+        self.mkm.load_my_key()
+        self.pm.load_installed_plugins()
 
-def page_back(page: ft.Page) -> None:
-    page.clean()
-    show_main_page(page)
+    def show_main_page(self) -> None:
+        def pick_file_and_install(e: ft.FilePickerResultEvent):
+            self.pm.install_plugin(e)
+
+        # プラグインをインストールするボタンを表示する
+        file_picker = ft.FilePicker(on_result=pick_file_and_install)
+        self.page.overlay.append(file_picker)
+        install_button = ft.ElevatedButton("Install Plugin", icon=ft.icons.UPLOAD_FILE, on_click=lambda _:file_picker.pick_files())
+        self.page.add(install_button)
+        self.page.update()
+
+    def page_back(self) -> None:
+        self.page.clean()
+        self.show_main_page()
+
 
 def main(page: ft.Page) -> None:
-    show_main_page(page)
+    app = MyApp(page)
+    app.show_main_page()
 
 if __name__ == "__main__":
     ft.app(target=main)
--- plugin_sample/sample_plugin.py
+++ plugin_sample/sample_plugin.py
@@ -2,7 +2,7 @@ import flet as ft
 
 def build_ui(page: ft.Page, function_to_previous_page):
     greeting_text = ft.Text("Welcome to the Plugin Page!", size=20)
-    back_button = ft.ElevatedButton("Back to Main Page", on_click=lambda _: function_to_previous_page(page))
+    back_button = ft.ElevatedButton("Back to Main Page", on_click=lambda _: function_to_previous_page())
 
     # 既存のUIをクリアして、新しいUIをページに追加
     page.clean()
