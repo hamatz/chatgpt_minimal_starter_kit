378d52c1afe60f9783139ccdbec897252e66b8e5
Makoto HAMATSU <hamatsu.m@googlemail.com>
2024-02-23 14:36:24 +0900

    ビルド時に設定する情報の定義ファイルを追加

--- build_on_mac.sh
+++ build_on_mac.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 ./install_preparation_for_mac.sh
 
+# JSONファイルのパス
+settings_file='build_settings.json'
+
+# バージョン情報をJSONファイルから取得
+build_version=$(cat settings_file | jq -r '.build_version')
+build_number=$(cat settings_file | jq -r '.build_number')
+project_name=$(cat settings_file | jq -r '.project_name')
+description=$(cat settings_file | jq -r '.description')
+product_name=$(cat settings_file | jq -r '.product_name')
+org_name=$(cat settings_file | jq -r '.org_name')
+company_name=$(cat settings_file | jq -r '.company_name')
+copyright=$(cat settings_file | jq -r '.copyright')
+
 echo "Starting app build process with additional arguments: $@"
-flet build $@
+flet build $@ --build-version $build_version -build-number $build_number --project $project_name --description $description --product $product_name --org $org_name --company $company_name --copyright $copyright
 
 echo "App build process completed."
--- build_on_win.sh
+++ build_on_win.sh
@@ -1,7 +1,19 @@
 # install_preparation_for_windows.ps1 を実行して準備
 .\install_preparation_for_win.ps1
 
+$settingsFile = 'build_settings.json'
+
+$jsonContent = Get-Content $settingsFile | ConvertFrom-Json
+$buildVersion = $jsonContent.build_version
+$buildNumber = $jsonContent.build_number
+$projectName = $jsonContent.project_name
+$description = $jsonContent.description
+$productName = $jsonContent.product_name
+$orgName = $jsonContent.org_name
+$companyName = $jsonContent.company_name
+$copyright = $jsonContent.copyright
+
 Write-Host "Starting app build process with additional arguments: $args"
-flet build $args
+flet build $args --build-version $buildVersion --build-number $buildNumber --project $projectName --description $description --product $productName --org $orgName --company $companyName --copyright $copyright
 
 Write-Host "App build process completed."
