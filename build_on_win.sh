dbc943563e903864921b418c5fbb8a24440804f1
Makoto HAMATSU <hamatsu.m@googlemail.com>
2024-02-23 14:47:50 +0900

    エントリポイントのパラメータも追加

--- build_on_win.sh
+++ build_on_win.sh
@@ -7,6 +7,7 @@ $jsonContent = Get-Content $settingsFile | ConvertFrom-Json
 $buildVersion = $jsonContent.build_version
 $buildNumber = $jsonContent.build_number
 $projectName = $jsonContent.project_name
+$moduleName = $jsonContent.module_name
 $description = $jsonContent.description
 $productName = $jsonContent.product_name
 $orgName = $jsonContent.org_name
@@ -14,6 +15,6 @@ $companyName = $jsonContent.company_name
 $copyright = $jsonContent.copyright
 
 Write-Host "Starting app build process with additional arguments: $args"
-flet build $args --build-version $buildVersion --build-number $buildNumber --project $projectName --description $description --product $productName --org $orgName --company $companyName --copyright $copyright
+flet build $args --build-version $buildVersion --build-number $buildNumber --project $projectName --description $description --product $productName --org $orgName --company $companyName --copyright $copyright --module-name $moduleName
 
 Write-Host "App build process completed."
