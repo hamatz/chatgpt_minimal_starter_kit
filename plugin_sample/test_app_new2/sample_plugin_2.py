83f3c117f79dd846e377375578125e7bb81c1394
Makoto HAMATSU <hamatsu.m@googlemail.com>
2024-02-25 23:15:28 +0900

    fletチュートリアルの計算機が簡単に移植できたのでサンプルアプリを入れ替え

--- plugin_sample/test_app_new2/sample_plugin_2.py
+++ plugin_sample/test_app_new2/sample_plugin_2.py
@@ -1,5 +1,267 @@
+import base64
+import os
 import flet as ft
 from plugin_interface import PluginInterface
+from flet import (
+    Column,
+    Container,
+    ElevatedButton,
+    Page,
+    Row,
+    Text,
+    UserControl,
+    border_radius,
+    colors,
+)
+
+class CalculatorApp(UserControl):
+    def build(self):
+        self.reset()
+        self.result = Text(value="0", color=colors.WHITE, size=20)
+        return Container(
+            width=300,
+            bgcolor=colors.BLACK,
+            border_radius=border_radius.all(20),
+            padding=20,
+            content=Column(
+                controls=[
+                    Row(controls=[self.result], alignment="end"),
+                    Row(
+                        controls=[
+                            ElevatedButton(
+                                text="AC",
+                                bgcolor=colors.BLUE_GREY_100,
+                                color=colors.BLACK,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="AC",
+                            ),
+                            ElevatedButton(
+                                text="+/-",
+                                bgcolor=colors.BLUE_GREY_100,
+                                color=colors.BLACK,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="+/-",
+                            ),
+                            ElevatedButton(
+                                text="%",
+                                bgcolor=colors.BLUE_GREY_100,
+                                color=colors.BLACK,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="%",
+                            ),
+                            ElevatedButton(
+                                text="/",
+                                bgcolor=colors.ORANGE,
+                                color=colors.WHITE,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="/",
+                            ),
+                        ],
+                    ),
+                    Row(
+                        controls=[
+                            ElevatedButton(
+                                text="7",
+                                bgcolor=colors.WHITE24,
+                                color=colors.WHITE,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="7",
+                            ),
+                            ElevatedButton(
+                                text="8",
+                                bgcolor=colors.WHITE24,
+                                color=colors.WHITE,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="8",
+                            ),
+                            ElevatedButton(
+                                text="9",
+                                bgcolor=colors.WHITE24,
+                                color=colors.WHITE,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="9",
+                            ),
+                            ElevatedButton(
+                                text="*",
+                                bgcolor=colors.ORANGE,
+                                color=colors.WHITE,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="*",
+                            ),
+                        ]
+                    ),
+                    Row(
+                        controls=[
+                            ElevatedButton(
+                                text="4",
+                                bgcolor=colors.WHITE24,
+                                color=colors.WHITE,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="4",
+                            ),
+                            ElevatedButton(
+                                text="5",
+                                bgcolor=colors.WHITE24,
+                                color=colors.WHITE,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="5",
+                            ),
+                            ElevatedButton(
+                                text="6",
+                                bgcolor=colors.WHITE24,
+                                color=colors.WHITE,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="6",
+                            ),
+                            ElevatedButton(
+                                text="-",
+                                bgcolor=colors.ORANGE,
+                                color=colors.WHITE,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="-",
+                            ),
+                        ]
+                    ),
+                    Row(
+                        controls=[
+                            ElevatedButton(
+                                text="1",
+                                bgcolor=colors.WHITE24,
+                                color=colors.WHITE,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="1",
+                            ),
+                            ElevatedButton(
+                                text="2",
+                                bgcolor=colors.WHITE24,
+                                color=colors.WHITE,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="2",
+                            ),
+                            ElevatedButton(
+                                text="3",
+                                bgcolor=colors.WHITE24,
+                                color=colors.WHITE,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="3",
+                            ),
+                            ElevatedButton(
+                                text="+",
+                                bgcolor=colors.ORANGE,
+                                color=colors.WHITE,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="+",
+                            ),
+                        ]
+                    ),
+                    Row(
+                        controls=[
+                            ElevatedButton(
+                                text="0",
+                                bgcolor=colors.WHITE24,
+                                color=colors.WHITE,
+                                expand=2,
+                                on_click=self.button_clicked,
+                                data="0",
+                            ),
+                            ElevatedButton(
+                                text=".",
+                                bgcolor=colors.WHITE24,
+                                color=colors.WHITE,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data=".",
+                            ),
+                            ElevatedButton(
+                                text="=",
+                                bgcolor=colors.ORANGE,
+                                color=colors.WHITE,
+                                expand=1,
+                                on_click=self.button_clicked,
+                                data="=",
+                            ),
+                        ]
+                    ),
+                ],
+            ),
+        )
+    def button_clicked(self, e):
+        data = e.control.data
+        if self.result.value == "Error" or data == "AC":
+            self.result.value = "0"
+            self.reset()
+        elif data in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."):
+            if self.result.value == "0" or self.new_operand == True:
+                self.result.value = data
+                self.new_operand = False
+            else:
+                self.result.value = self.result.value + data
+        elif data in ("+", "-", "*", "/"):
+            self.result.value = self.calculate(
+                self.operand1, float(self.result.value), self.operator
+            )
+            self.operator = data
+            if self.result.value == "Error":
+                self.operand1 = "0"
+            else:
+                self.operand1 = float(self.result.value)
+            self.new_operand = True
+        elif data in ("="):
+            self.result.value = self.calculate(
+                self.operand1, float(self.result.value), self.operator
+            )
+            self.reset()
+        elif data in ("%"):
+            self.result.value = float(self.result.value) / 100
+            self.reset()
+        elif data in ("+/-"):
+            if float(self.result.value) > 0:
+                self.result.value = "-" + str(self.result.value)
+            elif float(self.result.value) < 0:
+                self.result.value = str(
+                    self.format_number(abs(float(self.result.value)))
+                )
+        self.update()
+
+    def format_number(self, num):
+        if num % 1 == 0:
+            return int(num)
+        else:
+            return num
+
+    def calculate(self, operand1, operand2, operator):
+        if operator == "+":
+            return self.format_number(operand1 + operand2)
+        elif operator == "-":
+            return self.format_number(operand1 - operand2)
+        elif operator == "*":
+            return self.format_number(operand1 * operand2)
+        elif operator == "/":
+            if operand2 == 0:
+                return "Error"
+            else:
+                return self.format_number(operand1 / operand2)
+
+    def reset(self):
+        self.operator = "+"
+        self.operand1 = 0
+        self.new_operand = True
 
 class SamplePlugin2(PluginInterface):
     _instance = None
@@ -11,14 +273,19 @@ class SamplePlugin2(PluginInterface):
         return cls._instance
 
     def load(self, page: ft.Page, function_to_top_page, my_app_path: str):
-        my_header_cmp = self.ui_manager.get_component("simple_header")
-        my_header_instance = my_header_cmp(ft.icons.TABLE_RESTAURANT, "Sample Plugin v.0.0.1", "#20b2aa")
+        my_header_cmp = self.ui_manager.get_component("simple_header2")
+        icon_path = os.path.join(my_app_path, "back_button.png")
+        with open(icon_path, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+            app_icon = ft.Image(src_base64=encoded_string, width=30, height=30)
+        clickable_icon = ft.GestureDetector(
+            content=app_icon,
+            on_tap= lambda _: function_to_top_page()
+        )
+        my_header_instance = my_header_cmp(clickable_icon, "シンプル計算機 v.0.0.1", "#20b2aa")
         my_header_widget = my_header_instance.get_widget()
-        greeting_text = ft.Text("Welcome to the Plugin Page!", size=20)
-        back_button = ft.ElevatedButton("Back to Main Page", on_click=lambda _: function_to_top_page())
-
-        # 既存のUIをクリアして、新しいUIをページに追加
         page.clean()
         page.add(my_header_widget)
-        page.add(greeting_text, back_button)
+        calc = CalculatorApp()
+        page.add(calc)
         page.update()
