9b375d32e6770b2abe7133f1ad55082c100555cd
Makoto HAMATSU <hamatsu.m@googlemail.com>
2024-03-14 23:03:33 +0900

    my_key_managerのテストのアップデート

--- tests/test_my_key_manager.py
+++ tests/test_my_key_manager.py
@@ -1,5 +1,6 @@
+from datetime import datetime, timedelta
 import unittest
-from unittest.mock import patch
+from unittest.mock import MagicMock, mock_open, patch
 from my_key_manager import MyKeyManager
 import flet as ft
 import os
@@ -7,7 +8,7 @@ import os
 class TestMyKeyManager(unittest.TestCase):
 
     def setUp(self):
-        self.page = None
+        self.page = MagicMock() 
         self.ui_manager = None
         self.base_dir = os.path.dirname(os.path.abspath(__file__))
         self.my_key_file_name = "test_my_key.json"
@@ -32,7 +33,42 @@ class TestMyKeyManager(unittest.TestCase):
         incorrect_password = "wrong_password"
         result = self.key_manager.load_my_key(incorrect_password)
         self.assertFalse(result)
-        mock_print.assert_called_with("Failed to load key: 'NoneType' object has no attribute 'snack_bar'")
+        mock_print.assert_called_with('password error')
+
+    @patch("hashlib.sha256")
+    def test_compute_hash(self, mock_sha256):
+        mock_sha256.return_value.hexdigest.return_value = "fake_hash"
+        result = self.key_manager._MyKeyManager__compute_hash(b"data")
+        self.assertEqual(result, "fake_hash")
+        mock_sha256.assert_called_once_with(b"data")
+
+    @patch("cryptography.hazmat.primitives.kdf.pbkdf2.PBKDF2HMAC.derive")
+    def test_derive_key(self, mock_derive):
+        mock_derive.return_value = b"derived_key"
+        result = self.key_manager._MyKeyManager__derive_key(b"password", b"salt")
+        self.assertEqual(result, b"derived_key")
+
+    @patch('my_key_manager.datetime')
+    def test_account_lockout(self, mock_datetime):
+        # テスト開始時の時刻を設定
+        start_time = datetime(2020, 1, 1)
+        mock_datetime.now.return_value = start_time
+        manager = MyKeyManager(MagicMock() , None, os.path.dirname(os.path.abspath(__file__)), "test_my_key.json")
+        # 誤ったパスワードで5回ログインを試みる
+        for _ in range(5):
+            result = manager.load_my_key("wrong_password")
+            self.assertFalse(result)  # ログインは失敗するはず
+        # この時点でアカウントはロックされているはず
+        self.assertIsNotNone(manager.locked_until)
+        # ロックされた後の時刻をシミュレート
+        mock_datetime.now.return_value = start_time + timedelta(minutes=11)
+        # ロックが解除された後に正しいパスワードでログインを試みる
+        correct_password = "correct_password"
+        manager._MyKeyManager__generate_my_key(correct_password)
+        result = manager.load_my_key(correct_password)
+        # ログインが成功するはず
+        self.assertTrue(result)
+
 
 if __name__ == '__main__':
     unittest.main()
