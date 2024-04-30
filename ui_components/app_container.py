4b750ca0dc4b48fbc5ee2b376c1c6045cc66d3a8
Makoto HAMATSU <hamatsu.m@googlemail.com>
2024-04-30 19:30:10 +0900

    ホーム画面でシステムプラグインを囲む緑の線の幅を常に一定になるよう修正

--- ui_components/app_container.py
+++ ui_components/app_container.py
@@ -4,12 +4,16 @@ class AppContainer:
     def __init__(self, title_text: str, version_text: str, image: ft.GestureDetector, color) -> ft.Container:
         self.container = ft.Container(
         content=ft.Column([
-            image,
+            ft.Container(
+                content=image,
+                padding=1,
+                border=ft.border.all(1, color),
+                border_radius=ft.border_radius.all(5),
+            ),
             ft.Text(title_text, size=10),
             ft.Text(version_text, size=8),
         ], horizontal_alignment= ft.CrossAxisAlignment.CENTER),
         alignment=ft.alignment.center,
-        bgcolor=color,
         padding=2,
         margin=3,
     )
