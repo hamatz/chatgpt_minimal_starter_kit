8c509a2662ae8f5045593fe11590ec23f937f117
Makoto HAMATSU <hamatsu.m@googlemail.com>
2024-05-04 13:46:48 +0900

    システムUIコンポーネントを ft.UserControl を使う形で統一

--- ui_components/simple_header_2.py
+++ ui_components/simple_header_2.py
@@ -1,16 +1,26 @@
 import flet as ft
 
-class SimpleHeader2:
-    def __init__(self, icon: ft.GestureDetector, title_text: str, color) -> ft.Container:
-        self.header = ft.Container(
-        content=ft.Row([
-            icon,
-            ft.Text(title_text, size=20, color=ft.colors.WHITE),
-        ], alignment="left"),
-        bgcolor=color,
-        padding=10,
-        border_radius=ft.border_radius.all(10),
-    )
+class SimpleHeader2(ft.UserControl):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.icon = kwargs.get("icon")
+        self.title_text = kwargs.get("title_text")
+        self.color = kwargs.get("color")
 
-    def get_widget(self):
-        return self.header
+    def build(self):
+        icon = self.icon
+        if isinstance(icon, dict):
+            content = ft.Image(src_base64=icon["content"]) if icon["content"] else None
+            on_tap = eval(icon["on_tap"]) if icon["on_tap"] else None
+            icon = ft.GestureDetector(content=content, on_tap=on_tap)
+        title_text = self.title_text
+        color = self.color
+        return ft.Container(
+            content=ft.Row([
+                icon,
+                ft.Text(title_text, size=20, color=ft.colors.WHITE),
+            ], alignment="left"),
+            bgcolor=color,
+            padding=10,
+            border_radius=ft.border_radius.all(10),
+        )
